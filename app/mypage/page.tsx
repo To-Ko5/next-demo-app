0618fcdf8a48b055d2eda0a9394a845c935f2780
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-01-01 11:14:37 +0900

    feat: add button

--- app/mypage/page.tsx
+++ app/mypage/page.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { Button } from '@/components/ui/button'
 
 const Page = () => {
-  return <div>page</div>
+  return (
+    <>
+      <Button variant="outline">Click me</Button>
+    </>
+  )
 }
 
 export default Page
