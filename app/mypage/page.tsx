06dcbe7d5df3ea7342ae3bc830702a1980f9c82a
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-01-03 18:24:50 +0900

    feat: add dialog

--- app/mypage/page.tsx
+++ app/mypage/page.tsx
@@ -1,10 +1,38 @@
 import React from 'react'
 import { Button } from '@/components/ui/button'
+import { AlertDialog } from '@/components/ui/alert-dialog'
+import {
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger
+} from '@/components/ui/alert-dialog'
 
 const Page = () => {
   return (
     <>
-      <Button variant="outline">Click me</Button>
+      <AlertDialog>
+        <AlertDialogTrigger>
+          <Button variant="outline">Click me</Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction>Continue</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </>
   )
 }
