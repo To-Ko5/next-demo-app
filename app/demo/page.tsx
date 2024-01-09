2fe9a0fa83662dd37c33d8ffdbb4190b41b91534
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-01-09 18:54:19 +0900

    feat: basis change

--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -104,7 +104,7 @@ const Page = () => {
         >
           <CarouselContent>
             {Array.from({ length: 5 }).map((_, index) => (
-              <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+              <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/2">
                 <div className="p-1">
                   <Card>
                     <CardContent className="flex aspect-square items-center justify-center p-6">
